b47453aef2cb7832a05cd83a55ae9ccfdf844f46
rrosztoczy <rrosztoczy@gmail.com>
2019-07-18 14:36:58 -0600

    switched to in memory db

--- src/index.js
+++ src/index.js
@@ -5,26 +5,35 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const {startDatabase} = require('./database/mongo');
+const {insertAd, getAds} = require('./database/ads');
 
 const app = express();
 const port = process.env.PORT || "8000";
 
 // temp db
-const tempDb = [
+const ads = [
     {title: 'Hello, world (again)!'}
   ];
 
 //   configure app
-app.use(bodyParser.json())
-app.use(cors())
-app.use(helmet())
-app.use(morgan('combined'))
+app.use(bodyParser.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('combined'));
 
 
-app.get('/', (req, res) => {
-    res.status(200).send(tempDb);
+app.get('/', async (req, res) => {
+    res.send(await getAds());
 });
 
+//   start in memory db
+startDatabase().then(async () => {
+await insertAd({title: 'Hello, now from the in-memory database!'});
+
+// start the server
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`)
-})
+});
+});
+
